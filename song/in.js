83145af022d0684fb9819b939be8818501f254a7
Anju George <anjugeorgeku1@gmail.com>
2023-01-26 21:49:05 +0530

    azure

---
+++ song/in.js
@@ -0,0 +1,11 @@
+
+let laps=0;
+function ind(){
+    for(let i=1; i<5;i++)
+    { 
+        laps=laps+1;
+    }
+    console.log(laps);
+ind()
+
+}
---
+++ song/index.js
@@ -0,0 +1,10 @@
+
+
+function bottles(){
+     for(let i=99; i<1; i--)
+     {
+        console.log( "i");  
+     }
+       // console.log("take one down and pass it around ", num-1 +);
+}
+bottles()
