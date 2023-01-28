d597d04afb1c5c783249e455d748e253e46ed9d1
Anju George <anjugeorgeku1@gmail.com>
2023-01-28 16:22:39 +0530

    proj17

--- js_proj/js_intro/index.js
+++ js_proj/js_intro/index.js
@@ -3,12 +3,74 @@
 if (js === "amaze") {
     alert("hai world!")
 }*/
-
+/*
 let javaScript=true;
 console.log(typeof javaScript); //---type is boolean
 
 javaScript="Yes!";
 console.log(typeof(javaScript));  //--type is string=>dynamic typing ---changing the type 
 
-console.log(typeof 23);
+console.log(typeof 23);*/
+/*
+
+let markmass = 78;
+let markheight = 1.69;
+let markBMI = markmass / (markheight ** 2);
+console.log(markBMI);
+
+let johnMass = 92;
+let johnheight = 1.95;
+let johnBMI = johnMass / (johnheight ** 2);
+console.log(johnBMI);
+
+let BMI = (markBMI > johnBMI);
+console.log(BMI);
+
+
+let firstname="anju";
+let birthyear=2000;
+let year=2023;
+let job="teacher";
+
+//let fname="i'm" + " " + firstname + " " + (year-birthyear) + " " + " year old" + job;
+//console.log(fname);
+
+let lname=`i'm ${firstname} ${year-birthyear} year old ${job}`;
+console.log(lname);
+
+
+console.log
+
+const age=15;
+if(age>=18){
+    console.log("you can take license");
+}
+else{
+    const yearleft=(18-age);
+    console.log(`you are too young to take a license. wait for  ${yearleft} years`);
+}
+*/
+
+let markmass = 78;
+let markheight = 1.69;
+let markBMI = markmass / (markheight ** 2);
+
+let johnMass = 92;
+let johnheight = 1.95;
+let johnBMI = johnMass / (johnheight ** 2);
+
+//console.log(markBMI);
+//console.log(johnBMI);
+if(markBMI>johnBMI){
+    console.log(`mark bmi ${markBMI} is greater than john bmi ${johnBMI}`);
+}
+else{
+    console.log(`john BMI ${johnBMI} is greater than mark BMI ${markBMI}`);
+}
+
+
+
+
+
+
 
