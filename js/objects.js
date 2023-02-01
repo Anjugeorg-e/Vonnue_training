49cf009eae49a8b573b7cc2eb950cee11cb39520
Anju George <anjugeorgeku1@gmail.com>
2023-02-01 18:05:40 +0530

    new projects js

---
+++ js/objects.js
@@ -0,0 +1,19 @@
+//combination of key value pairs
+//in array we wont be able to emntion the key name for each value
+//we will be using this for unstructured data
+//unlike array no need to worry about the index
+
+const anju = {
+    FirstName: 'Anju',
+    lastName: 'George',
+    age: 2023-2000,
+    friends: ['theres','anu']
+};
+
+//accessing data from object
+
+console.log(anju)
+//. notation
+console.log(anju.lastName)
+//bracket notation
+console.log(anju['lastName']);
