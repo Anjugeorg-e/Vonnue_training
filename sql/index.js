8539c200f326d5b31ab25b617b8f9c28319d235d
Anju George <anjugeorgeku1@gmail.com>
2023-02-23 17:29:07 +0530

    feat-312

---
+++ sql/index.js
@@ -0,0 +1,14 @@
+//sql allows to manipulate data from database
+//rdbms stands for relational database managment system
+//rdbms is the basis for  SQL
+//the data in rdbms is stored in db objects called tables.
+
+//table is a collection of related data entries and it consists of rows and columns
+
+//every table is broke up into smaller entities called  fields /fields are like heading of column
+//field is a column in a table that is designed to maintain specific information about every record in a table
+
+//a record also called as a row ,is each individual entry that exists in a table
+//a column 
+
+//
