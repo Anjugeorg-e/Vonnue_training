df338c02483f675b025e81e25575b218e0707931
Anju George <anjugeorgeku1@gmail.com>
2023-03-02 13:49:57 +0530

    new

--- js_proj/advanced/button/script.js
+++ js_proj/advanced/button/script.js
@@ -1,5 +1,23 @@
-var button=document.getElementById('hello');
-button.style.background='green'
-button.onclick= () => {
-    console.log(button.innerHTML)
-}
+(function(){
+    var button=document.getElementById('hello');
+    button.style.background='green'
+    button.onclick= () => {
+        console.log(button.innerHTML)
+    }
+
+})();
+//declare a function and inside bracket and accesssing it using empty brackets
+//self invocing function to avoid global scoping issues
+
+
+//old code below
+
+
+// function main(){
+//     var button=document.getElementById('hello');
+//     button.style.background='green'
+//     button.onclick= () => {
+//         console.log(button.innerHTML)
+//     }
+// }
+// main();
--- js_proj/advanced/button/script2.js
+++ js_proj/advanced/button/script2.js
@@ -1,5 +1,19 @@
-var button=document.getElementById('bye');
-button.style.background='red'
-button.onclick= () => {
-    console.log(button.innerHTML)
-}
+(function(){
+    var button=document.getElementById('bye');
+    button.style.background='red'
+    button.onclick= () => {
+        console.log(button.innerHTML)
+    }
+})();
+
+
+//to solve issues of global scoping we will use self invoking function
+
+// function main2(){
+//     var button=document.getElementById('bye');
+//     button.style.background='red'
+//     button.onclick= () => {
+//         console.log(button.innerHTML)
+//     }
+// }
+
